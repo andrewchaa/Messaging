11700df5994a907c23c4d86a8295c37269d5152a
Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
2012-05-05 11:53:02 +0100

    Fixed issues with missing path in constructor, and property filter to get working

--- HelloWorld/Receiver/Receiver/Consumer.cs
+++ HelloWorld/Receiver/Receiver/Consumer.cs
@@ -6,12 +6,14 @@ namespace Receiver
 {
     internal class Consumer
     {
-        private MessageQueue channel;
+        private readonly MessageQueue channel;
 
         public Consumer(string channelName)
         {
-            channel = new MessageQueue {Formatter = new XmlMessageFormatter(new[] {typeof (string)})};
-
+            //We need to identify how the message is formatted - xml is the default
+            channel = new MessageQueue(channelName) {Formatter = new XmlMessageFormatter(new[] {typeof (string)})};
+            //We want to trace message headers such as correlation id, so we need to tell MSMQ to retrieve those
+            channel.MessageReadPropertyFilter.SetAll();
         }
 
         public void Consume()
